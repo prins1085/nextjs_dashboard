45ab4dfe19200bc2012528476dd33e17f976f713
Prins Harkhani <harkhaniprins2001@gmail.com>
2023-06-20 18:17:36 +0530

    back to normal

--- next.config.js
+++ next.config.js
@@ -3,20 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-const withTM = require('next-transpile-modules')(['@nivo/colors']);
-
-module.exports = withTM({
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      util: require.resolve('util/'),
-      stream: require.resolve('stream-browserify'),
-      buffer: require.resolve('buffer/'),
-    };
-
-    return config;
-  },
-});
-
-
 module.exports = nextConfig
