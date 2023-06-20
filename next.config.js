5c557fa7a14a3f657867d3ab9614f77b3c317cf7
Prins Harkhani <harkhaniprins2001@gmail.com>
2023-06-20 18:11:21 +0530

    Change again configure file

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
 
 const withTM = require('next-transpile-modules')(['@nivo/colors']);
 
-module.exports = withTM();
+module.exports = withTM({
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      util: require.resolve('util/'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer/'),
+    };
+
+    return config;
+  },
+});
+
 
 module.exports = nextConfig
